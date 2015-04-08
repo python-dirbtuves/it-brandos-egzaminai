19b77447f8dcd36719371ef8171a937bb1b5a56c
Mantas <sirexas@gmail.com>
2015-04-08 14:13:12 +0300

    Patobulinimai naudojant f.read().splitlines()

--- 2014/bandomasis/u1/u1.py
+++ 2014/bandomasis/u1/u1.py
@@ -1,43 +1,43 @@
 #!/usr/bin/env python3
 
 
-def isvezioti_picas(f):
+def isvezioti_picas(lines):
     # Nuskaitome įvesties failo pirmą eilutę, kurioje saugomas užsakovų skaičius
     # ir dienos kilometrų planas.
-    uzsakovu_sk, km_planas = next(f).split()
-    uzsakovu_sk, km_planas = int(uzsakovu_sk), int(km_planas)
+    uzsakovu_sk, km_planas = map(int, lines.pop(0).split())
 
-    # Nustatomos pradinės kintamųjų reikšmės.
+    # Nustatomas pradinis atstumas, kuris yra lygus nuliui.
     atstumas = 0
-    liko_uzsakovu = uzsakovu_sk
 
     # Šio ciklo pagalba vykdomas picų išvežiojimas skaičiuojant nuvažiuotą
     # atstumą ir likusių užsakovų skaičių.
-    for liko_uzsakovu in reversed(range(uzsakovu_sk)):
+    for line in lines[:uzsakovu_sk]:
 
         # Nuskaitomos sekančio užsakovo koordinatės.
-        x, y = next(f).split()
-        x, y = int(x), int(y)
+        x, y = map(int, line.split())
 
         # Apskaičiuojamas nuvažiuotas atstumas.
-        atstumas = atstumas + (abs(x) + abs(y)) * 2
+        atstumas += (abs(x) + abs(y)) * 2
+
+        # Sumažiname likusių užsakovų sakičių
+        uzsakovu_sk -= 1
 
         # Jei dienos kilometų planas viršytas, nutraukimas ciklas ir grąžinami
         # rezultatai.
         if atstumas > km_planas:
             break
 
-    return liko_uzsakovu, atstumas
+    return uzsakovu_sk, atstumas
 
 
 def programa():
     # Nuskaitomi įvesties duomenys
     with open('U1.txt') as f:
-        liko_uzsakovu, atstumas = isvezioti_picas(f)
+        uzsakovu_sk, atstumas = isvezioti_picas(f.read().splitlines())
 
     # Įrašomi rezultatai
     with open('U1rez.txt', 'w') as f:
-        print(liko_uzsakovu, atstumas, file=f)
+        print(uzsakovu_sk, atstumas, file=f)
 
 
 if __name__ == '__main__':
