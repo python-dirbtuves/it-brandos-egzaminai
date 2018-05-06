2f937a987fcaa4f4525020f381123361a616c5e6
Tomas Čerkauskas <tomas@cerkauskas.lt>
2018-05-06 13:27:41 +0300

    add: 2015 pagrindinio egzamino pirmos užduoties sprendimas

---
+++ exams/2015/pagrindinis/u1/tests.py
@@ -0,0 +1,19 @@
+import pytest
+import u1
+
+
+@pytest.fixture
+def u1_data():
+    return [
+        ('6 3 2 8 0 5 4 9 1 3', '6 4 4 11 4 9 8 14 7 8 6 4 4 3 3 2 2 1 0 0'),
+        ('0 0 0 0 0 0 0 0 0 0', '0 1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1'),
+        ('10 9 8 7 6 5 4 3 2 1', '10 9 9 8 8 7 7 6 6 5 5 4 4 3 3 2 2 1 1 0'),
+        ('5 4 3 2 1 0 0 0 0 0', '5 5 5 5 5 5 5 6 6 7 7 7 6 6 5 5 4 3 2 1')
+    ]
+
+
+def test(path, u1_data):
+    for i, o in u1_data:
+        (path/'U1.txt').write(i)
+        u1.main(path)
+        assert (path/'U1rez.txt').read() == [o]
---
+++ exams/2015/pagrindinis/u1/u1.py
@@ -0,0 +1,31 @@
+def read_data(path):
+    data = (path / 'U1.txt').read()[0]
+    return list(map(int, data.split()))
+
+
+def simulate(data):
+    cups = list(map(lambda x: 10, range(0, 10)))
+    eat_log = list(map(lambda x: 0, range(0, 20)))
+
+    # Initial greedy and unfriendly eating of girlz
+    for i in range(0, 10):
+        eat_log[i] += data[i]
+        cups[i] -= data[i]
+
+    # Eat and pass, each girl
+    for i, cup in enumerate(cups):
+        for j in range(cup):
+            eat_log[i + j + 1] += 1
+
+    return eat_log
+
+
+def write_data(path, data):
+    (path / 'U1rez.txt').write(' '.join(map(str, data)))
+
+
+def main(path):
+    data = read_data(path)
+    result = simulate(data)
+    write_data(path, result)
+
