b026c26e7c294cbfcdb00c7b4bc7e583bacb850a
Mantas <sirexas@gmail.com>
2018-05-08 11:37:13 +0300

    Add links to solutions of some exams

--- exams/2006/pagrindinis/u1/u1.py
+++ exams/2006/pagrindinis/u1/u1.py
@@ -1,12 +1,14 @@
+from itertools import islice
+
+
 def main(path):
     with (path / 'Duom1.txt').open() as f:
-        lines = f.readlines()
-        k, *lines = lines
-        k = int(k)
-        R = 0
-        for line in lines[:k]:
+        R: float = 0
+        k = int(next(f))
+        for line in islice(f, k):
             n, *D = map(int, line.split())
-            L = sum(1/d for d in D[:n])
-            R += 1/L
+            L = sum(1 / d for d in D[:n])
+            R += 1 / L
+
     with (path / 'Rez1.txt').open('w') as f:
         f.write(str(round(R, 2)))
