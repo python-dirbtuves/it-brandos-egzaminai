2cd3326cc39815bfef0e1cac45ca34b680996200
Edvardas Baltūsis <edvardasbaltusis@gmail.com>
2018-09-23 22:15:54 +0300

    Refactor

--- exams/2006/pagrindinis/u2/u2.py
+++ exams/2006/pagrindinis/u2/u2.py
@@ -1,17 +1,22 @@
 from os.path import join
+from pathlib import Path
 
-estimate_travel_time_in_minutes = lambda distance, speed: round(float(distance) / int(speed) * 60)
-min_to_hmin = lambda minutes: (minutes // 60, minutes % 60)
+def estimate_travel_time_in_minutes(distance, speed):
+    return round(float(distance) / int(speed) * 60)
 
 
-def access_file(file_dir: str):
-    with open(join(file_dir, "Duom2.txt"), 'r', encoding='utf-8') as f:
+def min_to_hmin(minutes):
+    return (minutes // 60, minutes % 60)
+
+
+def access_file(file_dir: Path):
+    with open(join(file_dir / "Duom2.txt"), 'r', encoding='utf-8') as f:
         yield from f
 
 
-def main(data_file_dir: str):
+def main(data_file_dir: Path):
     lines = access_file(data_file_dir)
-    with open(join(data_file_dir, "Rez2.txt"), 'w', encoding='utf-8') as fout:
+    with open(join(data_file_dir / "Rez2.txt"), 'w', encoding='utf-8') as fout:
         average_speed, departure_hours, departure_minutes = next(lines).split()[1:]
         departure_time_in_minutes = int(departure_hours) * 60 + int(departure_minutes)
         for line in lines:
--- exams/2007/pagrindinis/u1/u1.py
+++ exams/2007/pagrindinis/u1/u1.py
@@ -1,30 +1,31 @@
 from os.path import join
+from pathlib import Path
 
-add_arrays = lambda x, y: [int(a) + int(b) for a, b in zip(x, y)]
+
+def add_arrays(x, y):
+    return [a + b for a, b in zip(x, y)]
 
 
-def access_file(file_dir: str):
-    with open(join(file_dir, "U1.txt"), 'r', encoding='utf-8') as f:
+def access_file(file_dir: Path):
+    with open(join(file_dir / "U1.txt"), 'r', encoding='utf-8') as f:
         yield from f
 
 
-def main(data_file_dir: str):
+def main(data_file_dir: Path):
     lines = access_file(data_file_dir)
-    _ = next(lines)
+    next(lines)
     muschroom_counts: dict = {}
-    days = []
 
     for line in lines:
-        if " 0 0 0" in line:
+        day, *mushrooms = map(int, line.split())
+        if sum(mushrooms) == 0:
             continue
 
-        days.append(int(line[:2]))
-        muschroom_counts[days[-1]] = add_arrays(muschroom_counts.get(days[-1], [0, 0, 0]), line.split()[1:])
+        muschroom_counts[day] = add_arrays(muschroom_counts.get(day, [0, 0, 0]), mushrooms)
 
-    d, m = 0, 0
-    with open(join(data_file_dir, "U1rez.txt"), 'w', encoding='utf-8') as fout:
-        for day in sorted(list(set(days))):
+    with open(join(data_file_dir / "U1rez.txt"), 'w', encoding='utf-8') as fout:
+        for day in sorted(muschroom_counts):
             mc = muschroom_counts[day]
-            print("{} {} {} {}".format(day, *mc), file=fout)
-            d, m = (d, m) if m >= sum(mc) else (day, sum(mc))
-        print(d, m, file=fout)
+            print(day, *mc, file=fout)
+        mushroom_sum, max_day = max((sum(m), d) for d, m in muschroom_counts.items())
+        print(max_day, mushroom_sum, file=fout)
